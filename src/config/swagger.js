f41fed75093246551705ac862497b93ec9704184
devyashrasela <devyashrasela@gmail.com>
2025-10-03 17:52:15 +0530

    Endpoint created

---
+++ src/config/swagger.js
@@ -0,0 +1,34 @@
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'BMS (Business Management System) API',
+            version: '1.0.0',
+            description: 'The official API documentation for the Business Management System backend.',
+        },
+        components: {
+            securitySchemes: {
+                bearerAuth: {
+                    type: 'http',
+                    scheme: 'bearer',
+                    bearerFormat: 'JWT',
+                },
+            },
+        },
+        security: [{
+            bearerAuth: [],
+        }],
+    },
+    apis: ['./src/api/**/*.js'],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+const setupSwagger = (app) => {
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+};
+
+export default setupSwagger;
